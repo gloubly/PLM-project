5bb6cf2086f342b4e7177857738dd3c25f005a53
gloubly <etienne.goury@gmail.com>
2024-11-08 14:14:35 +0100

    update

--- app.py
+++ app.py
@@ -1,19 +1,46 @@
-from flask import Flask, render_template
+import tkinter as tk
+import pymongo
 
-app = Flask(__name__)
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+database = mongo_client["db_plm"]["projects"]
 
-@app.route("/")
-def home():
-    return render_template("login_page.html")
+#https://stackoverflow.com/questions/17466561/what-is-the-best-way-to-structure-a-tkinter-application
+class App(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.login_page()
 
-app.run()
+    def clear_frame(self, frame):
+        for widget in frame.winfo_children():
+            widget.destroy()
 
-"""
-nom societe : MGO SA GROUP
-MGO S.A group, specialize in agrifood and perfumery products, with 2000
-people in the world.
-The group has 4 main suppliers of raw materials and 10 outlets including 6
-in France and 4 in Belgium. The Company has just bought two production
-sites in Asia for the Asian market and two production sites in Africa for the
-African market.
-"""
+    def login_page(self):
+        #clear()
+        login_frame = tk.Frame(self.parent)
+        error_var = tk.StringVar(login_frame, "")
+        login_frame.pack()
+        tk.Entry(main_window, )
+        tk.Label(login_frame, text="Username").pack()
+        username_entry = tk.Entry(login_frame)
+        username_entry.pack()
+        tk.Label(login_frame, text="Password").pack()
+        password_entry = tk.Entry(login_frame)
+        password_entry.pack()
+        tk.Button(login_frame, text="Login", command=lambda: self.login(username_entry.get(), password_entry.get(), error_var)).pack()
+        tk.Label(login_frame, textvariable=error_var).pack()
+
+    def login(self, username, password, stringvar):
+        if username!="" and password!="":
+            project = database.find_one({"users":{"$elemMatch":{"username":username, "password":password}}})
+            if project:
+                stringvar.set("")
+                print('yay')
+            else:
+                stringvar.set("error")
+                
+
+    
+main_window = tk.Tk()
+App(main_window).pack()
+main_window.mainloop()
