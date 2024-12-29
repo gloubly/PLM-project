1411eaefc799be2398a3e2f2c57b3ba04ac01cdb
gloubly <etienne.goury@gmail.com>
2024-12-29 17:07:45 +0100

    test file

---
+++ src/test.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class TestApp(tk.Tk):
+    def __init__(self, database, timeout=None, skip=False, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.database = database
+        self.wm_title("App")
+        self.state('zoomed')
+        self.notebook = ttk.Notebook(self)
+        self.notebook.pack(fill='both', expand=True)
+        if timeout is not None:
+            self.after(timeout, lambda:self.destroy()) # for testing
